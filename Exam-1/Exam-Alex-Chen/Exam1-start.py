9d5091e3406feafb3cb1057fbef5550c9e6a52c1
AChen24562 <54772925+AChen24562@users.noreply.github.com>
2020-10-01 18:46:21 -0400

    exam

---
+++ Exam-1/Exam-Alex-Chen/Exam1-start.py
@@ -0,0 +1,5 @@
+quote = "TO BE OR NOT TO BE"
+
+print(quote[6:].title())
+
+print(quote)
--- Exam-1/split-join.py
+++ Exam-1/split-join.py
@@ -1,2 +1,9 @@
-list = "Hello"
-print(list.split())
+strNum = '5, 10, 99, -4, 3'
+print(strNum[0], strNum[-1])
+print(strNum[:])
+print(strNum)
+print(strNum[-10: 20])
+
+print(strNum.split())
+x = "X".join(strNum)
+print(x)
