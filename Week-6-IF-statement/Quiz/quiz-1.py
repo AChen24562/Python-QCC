8ddfcf55ae5b17b7cded5b48e09175940f03b3a2
AChen24562 <54772925+AChen24562@users.noreply.github.com>
2020-10-13 18:55:10 -0400

    Update quiz-1.py

---
+++ Week-6-IF-statement/Quiz/quiz-1.py
@@ -0,0 +1,41 @@
+'''
+Print "Yes" if p is equal to j, otherwise print "No". Use the variables given below:
+p = 'python'
+j = 'java'
+
+Question 2
+Needs Grading
+Print "fruits" if x is greater than y, print "tech" if x is equal to y, otherwise print "both". Use the variables given below:
+x = 'apple'
+y = 'app'
+
+Question 3
+Needs Grading
+Print " Hi ET502" if e is less than t. Use the variables given below:
+e = 2
+t = 502
+'''
+p = 'python'
+j = 'java'
+
+if p == j:
+    print('yes')
+else:
+    print('no')
+print()
+
+x = 'apple'
+y = 'app'
+
+if x > y:
+    print("fruits")
+elif x == y:
+    print("tech")
+else:
+    print('both')
+
+print()
+e = 2
+t = 502
+if e < t:
+    print("Hi ET502")
