b89b9d9bb96232a08a99bb5bc60034be7276d5f0
AChen24562 <54772925+AChen24562@users.noreply.github.com>
2020-10-25 21:16:22 -0400

    Update Quiz-2-Lists.py

--- Quiz/Quiz-2-Lists.py
+++ Quiz/Quiz-2-Lists.py
@@ -5,9 +5,11 @@ numbers = [17, 36, 5, 24, 89]
 numbers.insert(1, "kiwi")
 
 # 3
-print(numbers[1])
-print(numbers[2])
-print(numbers[3])
+
+print(numbers)
+# Print 2 - 4
+print(numbers[1:4])
+
 
 # 4
 numbers[0] = 77
