c8a4935f8848e31560809cc3173ba6927340550c
AChen24562 <54772925+AChen24562@users.noreply.github.com>
2020-11-04 00:00:42 -0500

    asd

--- Week-9-Functions/2randomInt-sqrt.py
+++ Week-9-Functions/2randomInt-sqrt.py
@@ -1,5 +1,5 @@
-from random import *
-from math import *
+from random import randint
+from math import isqrt
 
 random_num = randint(0, 100)
 print(f"Square root of {random_num} = {isqrt(random_num)}")
