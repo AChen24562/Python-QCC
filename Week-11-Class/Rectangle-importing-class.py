5994a3eb7bbcc5f979ff92e6dc95054a3feb8543
AChen24562 <54772925+AChen24562@users.noreply.github.com>
2020-11-18 04:44:21 -0500

    class exercise finish

--- Week-11-Class/Rectangle-importing-class.py
+++ Week-11-Class/Rectangle-importing-class.py
@@ -1,4 +1,36 @@
-from Rectangle_class import Rectangle
+from Rectangle_class import Rectangle, Circle
+import math
 
-r1 = Rectangle(5, 2)
+r1 = Rectangle(5, 3)
 print(r1.display())
+r1.area()
+
+# Set new Width and print width
+print()
+r1.setWidth(2)
+r1.getWidth()
+
+
+# Set new Height and print height
+r1.setHeight(7)
+r1.getHeight()
+
+
+print()
+print(r1.display())
+r1.area()
+
+# Circle
+# When Circle Radius is default
+print("\nCircle:")
+c1 = Circle()
+c1.getRadius()
+c1.area()
+c1.circumference()
+
+print()
+# Circle radius set to 5
+c1.setRadius(5)
+c1.getRadius()
+c1.area()
+c1.circumference()
--- Week-11-Class/Rectangle_class.py
+++ Week-11-Class/Rectangle_class.py
@@ -1,13 +1,50 @@
+from math import pi
+
+
 class Rectangle:
 
     def __init__(self, width = 1, height=1):
         self.width = width
         self.height = height
 
+# Set new width/height
+    def setWidth(self, width):
+        self.width = width
+
+    def setHeight(self, height):
+        self.height = height
+
+# Display Info
+    def getWidth(self):
+        print(f"New Width is: {self.width}")
+
+    def getHeight(self):
+        print(f"New Height is: {self.height}")
+
     def display(self):
-        dimension = f"Width: {self.width}\nHeight: {self.height}\n"
+        dimension = f"Width: {self.width}\nHeight: {self.height}"
         return dimension
 
+# Area
+    def area(self):
+        print(f"Area of a Rectangle H:{self.height} x W:{self.width} = {self.height * self.width}")
 
-r1 = Rectangle(4, 5)
-r1.display()
+
+class Circle:
+
+    def __init__(self, radius=1):
+        self.radius = radius
+
+    def setRadius(self, radius):
+        self.radius = radius
+
+    def getRadius(self):
+        print(f"Radius is: {self.radius}")
+
+    def area(self):
+        area = pi * self.radius ** 2
+        print(f"Area of circle with radius '{self.radius}' is {round(area,2)}")
+
+    def circumference(self):
+        circumference = 2 * pi * self.radius
+        print(f"circumference with raidus '{self.radius} is {round(circumference, 2)}'")
